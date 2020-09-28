b6fb8100236b4fcfa2936abacb8dd7b9c055a5e3
mustafahadid <mustafahadid40@gmail.com>
2020-09-28 17:01:11 +0430

    new changes

---
+++ js/nav-progress.js
@@ -0,0 +1,17 @@
+window.addEventListener('scroll', 
+moveScrollIndicator); 
+
+const scrollIndicatorElt = 
+document.getElementById('scrollIndicator'); 
+
+const maxHeight = 
+window.document.body.scrollHeight 
+- window.innerHeight; 
+
+function moveScrollIndicator(e) { 
+const percentage =  
+((window.scrollY) / maxHeight) * 100; 
+
+scrollIndicatorElt.style.width 
+= percentage + '%'; 
+} 

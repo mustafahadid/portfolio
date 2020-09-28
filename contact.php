442c33afcc3af149194c66b8b9385eea8003783b
mustafahadid <mustafahadid40@gmail.com>
2020-09-28 21:37:47 +0430

    php section

--- contact.php
+++ contact.php
@@ -1,22 +1,5 @@
 <?php
-$action=$_REQUEST['action'];
-if ($action=="")    /* display the contact form */
-    {
-    ?>
-    <form  action="" method="POST" enctype="multipart/form-data">
-    <input type="hidden" name="action" value="submit">
-    Your name:<br>
-    <input name="name" type="text" value="" size="30"/><br>
-    Your email:<br>
-    <input name="email" type="text" value="" size="30"/><br>
-    Your message:<br>
-    <textarea name="message" rows="7" cols="30"></textarea><br>
-    <input type="submit" value="Send email"/>
-    </form>
-    <?php
-    } 
-else                /* send the submitted data */
-    {
+
     $name=$_REQUEST['name'];
     $email=$_REQUEST['email'];
     $message=$_REQUEST['message'];
@@ -30,5 +13,5 @@ else                /* send the submitted data */
         mail("mustafahadid40@gmail.com", $subject, $message, $from);
         echo "Email sent!";
         }
-    }  
+    
 ?>

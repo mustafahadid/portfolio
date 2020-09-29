b1723f3a6a8d2cf295b6a138aa97d2ae366caad7
mustafahadid <mustafahadid40@gmail.com>
2020-09-29 14:12:41 +0430

    new page

--- contact.php
+++ contact.php
@@ -34,3 +34,18 @@ if( empty($errors))
 	header('Location: contact-form-thank-you.html');
 } 
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
+<head>
+	<title>Contact form handler</title>
+</head>
+
+<body>
+<!-- This page is displayed only if there is some error -->
+<?php
+echo nl2br($errors);
+?>
+
+
+</body>
+</html>

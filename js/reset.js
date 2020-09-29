251cc05f85d8b70768e078cee7046ccd42e875f9
mustafahadid <mustafahadid40@gmail.com>
2020-09-29 14:01:51 +0430

    php changes

--- js/reset.js
+++
@@ -1,7 +0,0 @@
-function submitForm() { 
-    // Get the first form with the name 
-    var frm = document.getElementsByName('myform')[0]; 
-    frm.submit(); // Submit 
-    frm.reset();  // Reset 
-    
- } 

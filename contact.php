5ad570d2cee987730c857282dd3cd543cec9ae18
mustafahadid <mustafahadid40@gmail.com>
2020-09-29 19:17:15 +0430

    2

--- contact.php
+++ contact.php
@@ -1,16 +1,51 @@
-<?php
-    if (isset($_POST['submit'])) {
-        $name = $_POST['name'];
-        $mailFrom = $_POST['email'];
-        $message =$_POST['message'];
-        $subject =['Web mail'];
+<?php 
+$errors = '';
+$myemail = 'mustafahadid40@gmail.com';//<-----Put Your email address here.
+if(empty($_POST['name'])  || 
+   empty($_POST['email']) || 
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
+}
 
+$name = $_POST['name']; 
+$email_address = $_POST['email']; 
+$message = $_POST['message']; 
 
-        $mailTo = "mustafahadid40@gmail.com";
-        $headers = "From: ".$mailFrom;
-        $txt = "You have recieved an email from".$name.".\n\n".$message;
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
 
-        mail($mailTo, $subject, $txt, $headers);
-        header("Location: index.php?mailsend");
-    }
+if( empty($errors))
+{
+	$to = $myemail; 
+	$email_subject = "Contact form submission: $name";
+	$email_body = "You have received a new message. ".
+	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message"; 
+	
+	$headers = "From: $myemail\n"; 
+	$headers .= "Reply-To: $email_address";
+	
+	mail($to,$email_subject,$email_body,$headers);
+	//redirect to the 'thank you' page
+	header('Location: contact-form-thank-you.html');
+} 
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
+<head>
+	<title>Contact form handler</title>
+</head>
+
+<body>
+<!-- This page is displayed only if there is some error -->
+<?php
+echo nl2br($errors);
+?>
+
+
+</body>
+</html>
